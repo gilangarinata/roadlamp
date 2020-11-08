408ae418d680f465c46ebd805a9656164753861c
gilangarinata <gilangarinata@gmail.com>
2020-11-08 14:58:12 +0700

    asas

--- api/routes/schedule.js
+++ api/routes/schedule.js
@@ -6,9 +6,9 @@ router.get("/deleteall/:key", ScheduleController.schedule_delete_all);
 
 router.get("/deletes/:id", ScheduleController.schedule_delete);
 
-router.get("/:userId/:hardwareId", ScheduleController.schedule_get);
-
 router.get("/get_by_device/:hardwareId", ScheduleController.schedule_get_by_device);
+
+router.get("/:userId/:hardwareId", ScheduleController.schedule_get);
 
 router.post("/edit/:id", ScheduleController.schedule_edit);
 
