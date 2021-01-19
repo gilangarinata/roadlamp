2ef755cfda4e0ff6a87aedfee4b2c792340195e6
gilangarinata <gilangarinata@gmail.com>
2021-01-19 17:38:15 +0700

    as

--- server.js
+++ server.js
@@ -10,34 +10,34 @@ const server = http.createServer(app);
 process.env.TZ = 'Asia/Jakarta'
 
 cron.schedule('*/2 * * * * *', function() {
-            Schedule.find()
-                .exec()
-                .then(schedules => {
-                        var datetime = new Date();
-                        const minutesNow = datetime.getMinutes();
-                        const hoursNow = datetime.getHours();
+    Schedule.find()
+        .exec()
+        .then(schedules => {
+            var datetime = new Date();
+            const minutesNow = datetime.getMinutes();
+            const hoursNow = datetime.getHours();
 
-                        for (var i = 0; i < schedules.length; i++) {
-                            console.log(parseInt(schedules[i].hour + "  " + hoursNow + " ====  " + schedules[i].minute + +minutesNow);
-                                if (hoursNow === parseInt(schedules[i].hour)) {
-                                    if (minutesNow === parseInt(schedules[i].minute)) {
-                                        const hardwareId = schedules[i].hardwareId;
-                                        const brightness = schedules[i].brightness;
-                                        const updateOps = {
-                                            brightnessSchedule: brightness
-                                        }
-                                        Hardware.update({ hardwareId: hardwareId }, { $set: updateOps }).exec().then(result => {
-                                            console.log('success');
-                                        }).catch(err => {
-                                            console.log('failed');
-                                        });
-                                    }
-                                }
-                            }
-                        })
-                    .catch(err => {
-                        res.status(500).json({ error: err })
-                    });
-                });
+            for (var i = 0; i < schedules.length; i++) {
+                console.log(parseInt(schedules[i].hour) + "  " + hoursNow + " ====  " + schedules[i].minute + +minutesNow);
+                if (hoursNow === parseInt(schedules[i].hour)) {
+                    if (minutesNow === parseInt(schedules[i].minute)) {
+                        const hardwareId = schedules[i].hardwareId;
+                        const brightness = schedules[i].brightness;
+                        const updateOps = {
+                            brightnessSchedule: brightness
+                        }
+                        Hardware.update({ hardwareId: hardwareId }, { $set: updateOps }).exec().then(result => {
+                            console.log('success');
+                        }).catch(err => {
+                            console.log('failed');
+                        });
+                    }
+                }
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err })
+        });
+});
 
-        server.listen(port);
+server.listen(port);
