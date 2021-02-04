19185b1f4178f9cccb8f8cc862bbaa64c0705c47
gilangarinata <gilangarinata@gmail.com>
2021-02-04 16:27:42 +0700

    asas

--- api/controller/users.js
+++ api/controller/users.js
@@ -311,6 +311,7 @@ exports.users_get_goverment = (req, res, next) => {
                 var newUsers = [];
                 for (var i = 0; i < users.length; i++) {
                     if (users[i].name != null) {
+                        console.log(users[i].name);
                         if (users[i].name.toLowerCase().includes(query.toLowerCase())) {
                             newUsers.push(users[i]);
                         }
