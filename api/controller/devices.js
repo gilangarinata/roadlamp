8d16feda494776fa96d8eca593c7a0a8145d6ee9
gilangarinata <gilangarinata@gmail.com>
2021-06-13 17:28:02 +0700

    add segment update

--- api/controller/devices.js
+++ api/controller/devices.js
@@ -1085,11 +1085,7 @@ exports.devices_get_kwh_segmented = (res, req, next) => {
                 message: "Users Not Found."
             })
         }
-    }).catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    });;
+    });
 
     function fetchDevice4() {
         Device.find({ user: userIdSuperuser[i]._id }).populate('hardware').exec().then(device => {
