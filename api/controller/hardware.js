e7e2ea13cfcab679bf354e62f83d850220c25861
gilangarinata <gilangarinata@gmail.com>
2021-07-12 21:06:36 +0700

    as

--- api/controller/hardware.js
+++ api/controller/hardware.js
@@ -59,25 +59,26 @@ exports.hardware_update_hardware_v2 = (req, res, next) => {
     var centerHid = -1;
 
 
-    for (var i = 0; i < keys.length; i++) {
-        console.log("key : " + keys[i]);
-        var pInt = parseInt(keys[i]);
+    // for (var i = 0; i < keys.length; i++) {
+    //     console.log("key : " + keys[i]);
+    //     var pInt = parseInt(keys[i]);
 
-        console.log("int : " + pInt + "   hid: " + keys[i]);
+    //     console.log("int : " + pInt + "   hid: " + keys[i]);
 
-        pInts.push(pInt)
-    }
+    //     pInts.push(pInt)
+    // }
 
-    centerHid = Math.min.apply(null, pInts);
+    // centerHid = Math.min.apply(null, pInts);
 
 
 
-    for (var i = 0; i < keys.length; i++) {
-        var pInt = parseInt(keys[i]);
-        if (pInts == centerHid) {
-            apHid = keys[i];
-        }
-    }
+    // for (var i = 0; i < keys.length; i++) {
+    //     var pInt = parseInt(keys[i]);
+    //     if (pInts == centerHid) {
+    //         apHid = keys[i];
+    //     }
+    // }
+
 
 
     updateHardware(req.body[keys[i]].hardwareId);
