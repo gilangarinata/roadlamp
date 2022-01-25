38e49297e10e7e3e0b66c70af14e6d7220585e95
gilangarinata <gilangarinata@gmail.com>
2022-01-25 23:15:32 +0700

    as

--- api/controller/hardware.js
+++ api/controller/hardware.js
@@ -9,7 +9,7 @@ var lastNotif = "0";
 const request = require('request');
 const openWeatherKey = '815168ce4992ad1ee04830a8556bedf9';
 // const errorLog = require('../../logger/logger').errorlog;
-const successlog = require('../../logger/logger').successlog;
+const successlog = require('../../logger/logger2/logger').successlog;
 
 const TIME_LIMIT = 600000;
 
